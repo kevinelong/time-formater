363284575dc1f2218f2694a821d72a8df141bd58
Kevin Ernest Long <kevinelong@gmail.com>
2025-05-01 11:14:08 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.apache.commons.lang.time.DateUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        LocalDateTime ldt = LocalDateTime.parse("1970-01-01T12:34:56");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //display
+        DateTimeFormatter timeColonFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println(timeColonFormatter.format(ldt));
+
+        /// //
+//        for(var l:DateFormat.getAvailableLocales()){
+//            System.out.println(l);
+//        }
+
+        Locale loc = new Locale("en", "US");
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, loc);
+
+        String dateInString = "12/31";
+        try {
+            Date date = DateUtils.parseDate(dateInString, new String[] { "yyyy-MM-dd HH:mm:ss", "dd/MM" });
+            System.out.println(date.toString());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
     }
 }
